9e312b6585381723de1211719492438156cf9d12
reynaldo <reynaldo57@hotmail.com>
2025-04-27 12:39:46 -0500

    solo css  6 commit

--- store/forms.py
+++ store/forms.py
@@ -57,7 +57,7 @@ class UpdateUserForm(UserChangeForm):
 		self.fields['username'].widget.attrs['class'] = 'form-control'
 		self.fields['username'].widget.attrs['placeholder'] = 'User Name'
 		self.fields['username'].label = ''
-		self.fields['username'].help_text = '<span class="form-text text-muted"><small>Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.</small></span>'
+		self.fields['username'].help_text = '<span class="form-text style="color: white;"><small>Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.</small></span>'
 
 class UpdateProfileForm(forms.ModelForm):
     class Meta:
