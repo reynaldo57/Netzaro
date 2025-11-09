f18e0fceb8211321bcd1a164b194a26cc27cff41
reynaldo <reynaldo57@hotmail.com>
2025-11-09 12:58:45 -0500

    cambio de static Directorios donde buscará los archivos estáticos durante el desarrollo

--- ecom/settings.py
+++ ecom/settings.py
@@ -125,7 +125,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = ['static/']
+# Directorio donde Django reunirá los archivos estáticos para producción
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Directorios donde buscará los archivos estáticos durante el desarrollo
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
