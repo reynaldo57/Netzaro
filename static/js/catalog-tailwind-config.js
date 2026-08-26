8013be732a286416de711030186a57d4d3a179d5
reynaldo <reynaldo57@hotmail.com>
2026-08-26 10:11:47 -0500

    cambié el frontend general

---
+++ static/js/catalog-tailwind-config.js
@@ -0,0 +1,18 @@
+tailwind.config = {
+    important: true,
+    theme: {
+        extend: {
+            colors: {
+                antares: {
+                    bg: '#040914',
+                    card: '#0B132B',
+                    cardHover: '#162244',
+                    border: '#1E293B',
+                    orange: '#FF5722',
+                    orangeHover: '#E63946',
+                    blueBanner: '#0B132B'
+                }
+            }
+        }
+    }
+}
---
+++ static/js/tailwind-config.js
@@ -0,0 +1,19 @@
+tailwind.config = {
+    important: true,
+    theme: {
+        extend: {
+            colors: {
+                antares: {
+                    bg: '#040914',
+                    card: '#0B132B',
+                    hover: '#162244',
+                    border: '#1E293B',
+                    brand: '#8BB3D6',
+                    accentStart: '#FFB703',
+                    accentMid: '#FF5722',
+                    accentEnd: '#E63946',
+                }
+            }
+        }
+    }
+}
